43285b2e18eac7c77cf9cf09639f0d69a32800a2
Jana Settel <zettelyana@gmail.com>
2022-01-07 21:51:08 +0100

    Refactor soundNumber

--- lib/main.dart
+++ lib/main.dart
@@ -6,73 +6,69 @@ void main() => runApp(const XylophoneApp());
 class XylophoneApp extends StatelessWidget {
   const XylophoneApp({Key? key}) : super(key: key);
 
+  void playSound(int soundNumber) {
+    final audio = AudioCache();
+    audio.play('note$soundNumber.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: Column(
-              children: [
-                MaterialButton(
-                  color: Colors.red,
-                  enableFeedback: false,
-                  onPressed: () {
-                    final audio = AudioCache();
-                    audio.play('note1.wav');
-                  },
-                ),
-                MaterialButton(
-                  color: Colors.orange,
-                  enableFeedback: false,
-                  onPressed: () {
-                    final audio = AudioCache();
-                    audio.play('note2.wav');
-                  },
-                ),
-                MaterialButton(
-                  color: Colors.yellow,
-                  enableFeedback: false,
-                  onPressed: () {
-                    final audio = AudioCache();
-                    audio.play('note3.wav');
-                  },
-                ),
-                MaterialButton(
-                  color: Colors.green,
-                  enableFeedback: false,
-                  onPressed: () {
-                    final audio = AudioCache();
-                    audio.play('note4.wav');
-                  },
-                ),
-                MaterialButton(
-                  color: Colors.lightBlue[100],
-                  enableFeedback: false,
-                  onPressed: () {
-                    final audio = AudioCache();
-                    audio.play('note5.wav');
-                  },
-                ),
-                MaterialButton(
-                  color: Colors.blue,
-                  enableFeedback: false,
-                  onPressed: () {
-                    final audio = AudioCache();
-                    audio.play('note6.wav');
-                  },
-                ),
-                MaterialButton(
-                  color: Colors.indigo,
-                  enableFeedback: false,
-                  onPressed: () {
-                    final audio = AudioCache();
-                    audio.play('note7.wav');
-                  },
-                ),
-              ],
-            ),
+          child: Column(
+            children: [
+              MaterialButton(
+                color: Colors.red,
+                enableFeedback: false,
+                onPressed: () {
+                  playSound(1);
+                },
+              ),
+              MaterialButton(
+                color: Colors.orange,
+                enableFeedback: false,
+                onPressed: () {
+                  playSound(2);
+                },
+              ),
+              MaterialButton(
+                color: Colors.yellow,
+                enableFeedback: false,
+                onPressed: () {
+                  playSound(3);
+                },
+              ),
+              MaterialButton(
+                color: Colors.green,
+                enableFeedback: false,
+                onPressed: () {
+                  playSound(4);
+                },
+              ),
+              MaterialButton(
+                color: Colors.lightBlue[100],
+                enableFeedback: false,
+                onPressed: () {
+                  playSound(5);
+                },
+              ),
+              MaterialButton(
+                color: Colors.blue,
+                enableFeedback: false,
+                onPressed: () {
+                  playSound(6);
+                },
+              ),
+              MaterialButton(
+                color: Colors.indigo,
+                enableFeedback: false,
+                onPressed: () {
+                  playSound(7);
+                },
+              ),
+            ],
           ),
         ),
       ),
